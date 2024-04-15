3a44b4c6e3d398087cb875b72c74e124896d8434
Ayyoob Ajward <ajwardayyoob@gmail.com>
2024-04-15 22:42:58 +0530

    small changes

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,3 @@
 export const PORT = 5555;
 
+export const MONGODB_URI = 'mongodb+srv://user1:user1@cluster0.8rmis9d.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -79,7 +79,6 @@ router.put('/:id', async (req, res) => {
     }
 })
 
-
 router.delete('/:id', async (req, res) => {
     try {
         const { id } = req.params;
